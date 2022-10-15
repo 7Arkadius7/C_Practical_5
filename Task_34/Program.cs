f300f1ae14230e9bc3863130d284a254293e41c1
Sergey <motor-person@yandex.ru>
2022-10-16 01:48:16 +0400

    Сделали Task_34

---
+++ Task_34/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) System.Console.Write($"{array[i]}, ");
+        else System.Console.Write($"{array[i]}");
+    }
+    System.Console.Write("]");
+}
+
+int EvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] arr = CreateArrayRndInt(5, 100, 999);
+PrintArray(arr);
+System.Console.WriteLine();
+int evenNumbers = EvenNumbers(arr);
+System.Console.WriteLine($"Количество четных чисел в массиве -> {evenNumbers}");
