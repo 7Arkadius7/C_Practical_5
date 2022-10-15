b35c52f44e0170f109a4b7ac38fed90cf60584e5
Sergey <motor-person@yandex.ru>
2022-10-15 17:00:30 +0400

    Сделали Task_33

---
+++ Task_33/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число для поиска его в массиве");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) System.Console.Write($"{array[i]}, ");
+        else System.Console.Write($"{array[i]}");
+    }
+    System.Console.Write("]");
+}
+
+bool CheckNumber(int[] array, int arg)
+{
+    bool checknum = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == arg)
+        {
+            checknum = true;
+            break;
+        }
+    }
+    return checknum;
+
+}
+
+int[] arr = CreateArrayRndInt(12, -9, 10);
+PrintArray(arr);
+System.Console.WriteLine();
+bool checkNumber = CheckNumber(arr, number);
+if (checkNumber) System.Console.WriteLine("Да");
+else System.Console.WriteLine("Нет");
